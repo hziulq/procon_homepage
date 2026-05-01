79854e1b61d337aa9cdfa8d57d37eafa28fb241c
hziulq <kd1295154@st.kobedenshi.ac.jp>
2026-05-01 10:19:33 +0900

    log added in fetchinternalendpoint

--- app/lib/fetch-internal-endpoint.ts
+++ app/lib/fetch-internal-endpoint.ts
@@ -1,3 +1,5 @@
+import DevLog from "./dev-log";
+
 /**
  * 内部APIへリクエストを送信するヘルパー関数
  * @param method: string リクエストメソッド
@@ -8,11 +10,12 @@
  */
 export async function fetchInternalEndpoint(method: string, endpoint: string, body?: Record<string, unknown>) {
 
-    if (process.env.NODE_ENV === "development") {
-        console.log("method", method);
-    }
+    DevLog(`method: ${method}`);
+    DevLog(`endpoint: ${endpoint}`);
+    DevLog(`body: ${body}`);
 
     const apiUrl = process.env.NODE_ENV === "development" ? process.env.DEV_INTERNAL_API_URL : process.env.INTERNAL_API_URL;
+    DevLog(`apiUrl: ${apiUrl}`);
 
     if (!process.env.INTERNAL_API_SECRET) {
         throw new Error("INTERNAL_API_SECRET is not defined");
@@ -21,7 +24,7 @@ export async function fetchInternalEndpoint(method: string, endpoint: string, bo
     const bearerToken = process.env.INTERNAL_API_SECRET;
     const bypassSecret = process.env.VERCEL_AUTOMATION_BYPASS_SECRET;
 
-    return fetch(`${apiUrl}${endpoint}`, {
+    const response = await fetch(`${apiUrl}${endpoint}`, {
         method: method,
         headers: {
             "x-vercel-protection-bypass": bypassSecret || "",
@@ -30,5 +33,7 @@ export async function fetchInternalEndpoint(method: string, endpoint: string, bo
         },
         body: body ? JSON.stringify(body) : undefined,
     });
+    DevLog(`response: ${response}`);
+    return response;
 }
 
